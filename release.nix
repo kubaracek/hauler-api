f5117921df4bcc4c31739d86a45186cce60154b7
Marek Fajkus <marek.faj@gmail.com>
2020-04-04 20:02:51 +0200

    override persistent-postgresql

--- release.nix
+++ release.nix
@@ -1,4 +1,15 @@
 let
-  pkgs = import <nixpkgs> { };
-in
-  pkgs.haskellPackages.callPackage ./default.nix { }
+  config = {
+    allowBroken = true;
+    packageOverrides = pkgs: with pkgs.haskell.lib; rec {
+        haskellPackages = pkgs.haskell.packages.ghc865.override {
+            overrides = self: super: {
+                persistent-postgresql = dontCheck super.persistent-postgresql;
+                hauler-api = pkgs.haskellPackages.callPackage ./default.nix {};
+            };
+        };
+    };
+  };
+
+  pkgs = import <nixpkgs> { inherit config; };
+in pkgs.haskellPackages.hauler-api
